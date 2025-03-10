041568a3843a94c544f04c9dc4b85ee6d55e5b53
tamerhayek <dev@tamerhayek.com>
2025-03-10 11:36:16 +0100

    feat: Hero section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,26 @@
-<h1>Gla Stables Equitazione</h1>
+<script lang="ts">
+	import { page } from '$app/state';
+	import Hero from './Hero.svelte';
+</script>
+
+<svelte:head>
+	<title>Gla Stables Equitazione</title>
+	<meta name="title" content="Gla Stables Equitazione" />
+	<meta name="description" content="" />
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="{page.url.origin}{page.url.pathname}" />
+	<meta property="og:title" content="Gla Stables Equitazione" />
+	<meta property="og:description" content="" />
+	<meta property="og:image" content={`${page.url.origin}/logo.webp}`} />
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content="{page.url.origin}{page.url.pathname}" />
+	<meta property="twitter:title" content="Gla Stables Equitazione" />
+	<meta property="twitter:description" content="" />
+	<meta property="twitter:image" content={`${page.url.origin}/logo.webp}`} />
+</svelte:head>
+
+<Hero />
---
+++ src/routes/Hero.svelte
@@ -0,0 +1,14 @@
+<section id="hero" class="relative flex min-h-[70dvh] w-full flex-col items-center">
+	<div
+		class="absolute top-0 left-0 z-0 h-full w-full overflow-hidden rounded-b-4xl bg-[url(/assets/images/hero.webp)] bg-cover bg-center bg-no-repeat"
+	>
+		<div
+			class="absolute top-0 left-0 z-10 h-full w-full bg-gradient-to-b from-black/30 from-0% via-black/60 via-70% to-black/70 to-100%"
+		></div>
+	</div>
+	<div
+		class="z-20 flex h-full w-full max-w-6xl flex-col items-center justify-end gap-8 py-10 text-center lg:items-start lg:px-5 lg:pb-20"
+	>
+		<h1 class="text-5xl font-bold text-white capitalize lg:text-7xl">Gla Stables Equitazione</h1>
+	</div>
+</section>
