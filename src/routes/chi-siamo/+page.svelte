a11c8f0b84b35e82efd600f100abbc8f9ac9c4d7
tamerhayek <dev@tamerhayek.com>
2025-03-10 15:45:18 +0100

    fix: move heading before image for mobile responsiveness in chi-siamo route

--- src/routes/chi-siamo/+page.svelte
+++ src/routes/chi-siamo/+page.svelte
@@ -23,63 +23,66 @@
 </svelte:head>
 
 <div class="flex flex-col gap-40 py-40">
-	<section
-		id="gianluca-apolloni"
-		class="container-centered flex-col justify-between gap-10 lg:flex-row"
-	>
-		<div class="order-2 flex grow flex-col gap-3 lg:order-1">
-			<h2 class="text-primary-content w-full text-3xl font-bold">Gianluca Apolloni</h2>
-			<p class="prose lg:text-justify">
-				Nato a Tivoli il 22 Aprile del 1986 Gianluca è oggi cavaliere professionista di 2° grado
-				Discipline olimpiche .
-				<br /> <br />
-				Specializzato anche nei giovani cavalli è allevatore di giovani soggetti che porta dai primi
-				passi ai massimi livelli. Gianluca ha più volte rappresentato l'Italia all'estero. Istruttore
-				Federale di 2° livello, ad oggi conta numerosi allievi impegnati in gare a tutti i livelli.
-			</p>
-		</div>
-		<div class="order-1 flex grow flex-col gap-2 lg:order-2">
-			<img src="/assets/images/hero.webp" alt="Immagine" />
-		</div>
-	</section>
-
-	<section id="susan-hayek" class="container-centered flex-col justify-between gap-10 lg:flex-row">
-		<div class="flex grow flex-col gap-2">
-			<img src="/assets/images/sample.webp" alt="Immagine" />
-		</div>
-		<div class="flex grow flex-col gap-3">
-			<h2 class="text-primary-content w-full text-3xl font-bold">Susan Hayek</h2>
-			<p class="prose lg:text-justify">
-				Nata a Roma il 14 Marzo del 1990, diplomata al Liceo Classico Orazio di Roma, decide di
-				intraprendere la sua carriera lavorativa nel mondo dell'equitazione spinta dalla sua grande
-				passione per questo sport.
-				<br /> <br />
-				Amazzone di 1 Grado, Istruttore FISE 1 Livello, Tecnico di Volteggio ASI, Centro calcoli Salto
-				Ostacoli federale di 2 livello, Social Media Manager & Web site Developer del Longines Global
-				Champions Tour di Roma, ha collaborato 3 anni con la Federazione Italiana Sport Equestri Lazio
-				nel settore affiliazioni, tesseramento e reparto sportivo. Ha lavorato all'interno di comitati
-				organizzatori di molti eventi sportivi nazionali ed internazionali in diversi ruoli.
-				<br /> <br />
-				Ad oggi è assistente della Event Coordinator del LGCT di Roma.
-			</p>
+	<section id="gianluca-apolloni" class="container-centered flex-col gap-10">
+		<h2 class="text-primary-content w-full text-3xl font-bold">Gianluca Apolloni</h2>
+		<div class="flex flex-col gap-10 lg:flex-row">
+			<div class="order-2 flex grow flex-col gap-3 lg:order-1">
+				<p class="prose lg:text-justify">
+					Nato a Tivoli il 22 Aprile del 1986 Gianluca è oggi cavaliere professionista di 2° grado
+					Discipline olimpiche .
+					<br /> <br />
+					Specializzato anche nei giovani cavalli è allevatore di giovani soggetti che porta dai primi
+					passi ai massimi livelli. Gianluca ha più volte rappresentato l'Italia all'estero. Istruttore
+					Federale di 2° livello, ad oggi conta numerosi allievi impegnati in gare a tutti i livelli.
+				</p>
+			</div>
+			<div class="order-1 flex grow flex-col gap-2 lg:order-2">
+				<img src="/assets/images/hero.webp" alt="Immagine" />
+			</div>
 		</div>
 	</section>
 
-	<section id="serena-hayek" class="container-centered flex-col justify-between gap-10 lg:flex-row">
-		<div class="order-2 flex grow flex-col gap-3 lg:order-1">
-			<h2 class="text-primary-content w-full text-3xl font-bold">Serena Hayek</h2>
-			<p class="prose lg:text-justify">
-				Nata il 05 Maggio del 1998, Serena è presente al maneggio in veste di segretaria. Diplomata
-				al liceo artistico S.Orsola, ad oggi studente universitaria della facoltá di Cooperazione
-				Internazionale a La Sapienza.
-				<br /> <br />
-				Centro calcoli federale di 1 Livello, Social Media Manager dell'amazzone juniores Melissa Ambrosetti,
-				Speaker di concorsi di salto ostacoli nazionali e internazionali, ha lavorato nell'ufficio stampa
-				dei Campionati Europei di Dressage e nel Longines Global Champions Tour di Roma
-			</p>
+	<section id="susan-hayek" class="container-centered flex-col gap-10">
+		<h2 class="text-primary-content w-full text-3xl font-bold">Susan Hayek</h2>
+		<div class="flex flex-col gap-10 lg:flex-row">
+			<div class="flex grow flex-col gap-2">
+				<img src="/assets/images/sample.webp" alt="Immagine" />
+			</div>
+			<div class="flex grow flex-col gap-3">
+				<p class="prose lg:text-justify">
+					Nata a Roma il 14 Marzo del 1990, diplomata al Liceo Classico Orazio di Roma, decide di
+					intraprendere la sua carriera lavorativa nel mondo dell'equitazione spinta dalla sua
+					grande passione per questo sport.
+					<br /> <br />
+					Amazzone di 1 Grado, Istruttore FISE 1 Livello, Tecnico di Volteggio ASI, Centro calcoli Salto
+					Ostacoli federale di 2 livello, Social Media Manager & Web site Developer del Longines Global
+					Champions Tour di Roma, ha collaborato 3 anni con la Federazione Italiana Sport Equestri Lazio
+					nel settore affiliazioni, tesseramento e reparto sportivo. Ha lavorato all'interno di comitati
+					organizzatori di molti eventi sportivi nazionali ed internazionali in diversi ruoli.
+					<br /> <br />
+					Ad oggi è assistente della Event Coordinator del LGCT di Roma.
+				</p>
+			</div>
 		</div>
-		<div class="order-1 flex grow flex-col gap-2 lg:order-2">
-			<img src="/assets/images/sample.webp" alt="Immagine" />
+	</section>
+
+	<section id="serena-hayek" class="container-centered flex-col gap-10">
+		<h2 class="text-primary-content w-full text-3xl font-bold">Serena Hayek</h2>
+		<div class="flex flex-col gap-10 lg:flex-row">
+			<div class="order-2 flex grow flex-col gap-3 lg:order-1">
+				<p class="prose lg:text-justify">
+					Nata il 05 Maggio del 1998, Serena è presente al maneggio in veste di segretaria.
+					Diplomata al liceo artistico S.Orsola, ad oggi studente universitaria della facoltá di
+					Cooperazione Internazionale a La Sapienza.
+					<br /> <br />
+					Centro calcoli federale di 1 Livello, Social Media Manager dell'amazzone juniores Melissa Ambrosetti,
+					Speaker di concorsi di salto ostacoli nazionali e internazionali, ha lavorato nell'ufficio
+					stampa dei Campionati Europei di Dressage e nel Longines Global Champions Tour di Roma
+				</p>
+			</div>
+			<div class="order-1 flex grow flex-col gap-2 lg:order-2">
+				<img src="/assets/images/sample.webp" alt="Immagine" />
+			</div>
 		</div>
 	</section>
 </div>
